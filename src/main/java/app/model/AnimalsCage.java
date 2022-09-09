0e59df87b64e334cee4b3521646a8ea2f03eadd8
Maria Filatova <m.bronskaya@gmail.com>
2022-09-09 18:17:13 +0300

    new commit: Тесты прошли, но с каким-то адским трейсом О_о

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,24 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
+    @Autowired
+    @Qualifier("timer")
+    private Timer time;
+
+    public Timer getTimer() {
+        return time;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
